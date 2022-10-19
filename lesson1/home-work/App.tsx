33490c5a93fb4ad284a8bc2058d637341a09a434
Vladimir Traihel <vovatraigel@gmail.com>
2022-10-20 01:38:52 +0300

    home-work

--- lesson1/home-work/App.tsx
+++ lesson1/home-work/App.tsx
@@ -1,20 +1,203 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import React, {useState} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    Dimensions,
+    TextInput,
+    TouchableOpacity,
+    Pressable,
+    ListRenderItem,
+    FlatList,
+    ScrollView
+} from 'react-native';
+
+const {width} = Dimensions.get('screen')
+
+const WIDTH = width
+
+const brands = new Array(4).fill(null).map((el, index) => ({
+    id: index + 1,
+    imgUrl: `./assets/brands/${index + 1}.jpg`
+}))
+
+type BrandsType = {
+    id: number
+    imgUrl: string
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [text, setText] = useState<string>('')
+
+    const renderItemHandler: ListRenderItem<BrandsType> = ({item}) => {
+        return <View style={{
+            borderWidth: 1,
+            borderRadius: 5,
+            margin: 5
+        }}>
+            <Image
+                style={{
+                    width: (WIDTH - 17.5 * 2) / 2 - 5,
+                    height: 35,
+                    paddingVertical: 30
+                }}
+                source={require(`./assets/brands/1.jpg`)}
+            />
+        </View>
+    }
+
+    return (
+        <View style={styles.container}>
+            <ScrollView style={styles.scroll}>
+                <View style={styles.headerImg}>
+                    <Pressable onPress={() => alert('Menu: Page is in development')}>
+                        <Image source={require('./assets/menu.png')}/>
+                    </Pressable>
+                    <Image source={require('./assets/logo.jpg')}/>
+                    <Pressable onPress={() => alert('User: Page is in development')}>
+                        <Image source={require('./assets/user.png')}/>
+                    </Pressable>
+                </View>
+                <View style={styles.searchBox}>
+                    <TextInput
+                        style={styles.inputSearch}
+                        onChangeText={setText}
+                        value={text}
+                        placeholder={'Search'}
+                    />
+                    <TouchableOpacity
+                        style={styles.buttonSearch}
+                        onPress={() => alert('Search: Page is in development')}
+                    >
+                        <Text style={styles.buttonSearchText}>&#128269;</Text>
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <View style={styles.titleLogo}>
+                        <Text style={styles.titleTextLogo}>Custom Wide Body Kits</Text>
+                    </View>
+                    <Image
+                        style={styles.imgLogo}
+                        source={require('./assets/slide1.jpg')}
+                    />
+                </View>
+                <View style={styles.brandsBox}>
+                    <Text style={styles.brandsTitle}>WE CARRY TOP BRANDS</Text>
+                    <View style={styles.lineTitle}></View>
+
+
+                </View>
+                <View style={styles.brandsFlatList}>
+                    <FlatList
+                        data={brands}
+                        renderItem={renderItemHandler}
+                        numColumns={2}
+                    />
+                </View>
+                <View>
+                    <Image
+                        style={styles.imgSpeakers}
+                        source={require('./assets/speakers.jpg')}
+                    />
+                    <Text></Text>
+                </View>
+                <View>
+                    <Image
+                        style={styles.imgSpeakers}
+                        source={require('./assets/speakers.jpg')}
+                    />
+                </View>
+            </ScrollView>
+            <StatusBar style="auto"/>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    scroll: {
+        // position: 'absolute',
+        // left: 12
+    },
+    headerImg: {
+        flexDirection: "row",
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        height: 55,
+        width: WIDTH - 30,
+        marginTop: 30,
+        marginBottom: 15,
+    },
+    searchBox: {
+        flexDirection: "row",
+        justifyContent: 'center',
+        width: WIDTH - 30,
+        height: 42,
+    },
+    inputSearch: {
+        height: 42,
+        width: WIDTH - 72,
+        borderWidth: 1,
+        padding: 10,
+    },
+    buttonSearch: {
+        width: 42,
+        height: 42,
+        backgroundColor: '#fff',
+        borderWidth: 1,
+        borderLeftWidth: 0
+    },
+    buttonSearchText: {
+        fontSize: 25,
+        textAlign: 'center'
+    },
+    titleLogo: {
+        alignItems: 'center',
+        position: "absolute",
+        zIndex: 2,
+        width: WIDTH - 30,
+        top: 60,
+        left: 0
+    },
+    titleTextLogo: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: "700",
+    },
+    imgLogo: {
+        width: WIDTH - 30,
+        height: 120,
+        marginVertical: 20,
+    },
+    brandsBox: {
+        width: WIDTH - 30,
+    },
+    brandsTitle: {
+        fontSize: 22,
+        fontWeight: '500',
+    },
+    lineTitle: {
+        backgroundColor: 'red',
+        width: 50,
+        height: 3,
+        marginTop: 10,
+        marginBottom: 15
+    },
+    brandsFlatList: {
+        justifyContent: 'center',
+        width: WIDTH - 20,
+    },
+    imgSpeakers: {
+        width: WIDTH - 20,
+        borderRadius: 5,
+        marginTop: 20,
+    }
 });
--- lesson1/my-app/App.tsx
+++ lesson1/my-app/App.tsx
@@ -2,16 +2,16 @@ import {StatusBar} from 'expo-status-bar';
 import React, {useEffect} from 'react';
 import {useState} from 'react';
 import {
-  ActivityIndicator,
-  Alert,
-  Button,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  TextInput,
-  FlatList, ListRenderItem, Dimensions
+    ActivityIndicator,
+    Alert,
+    Button,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableOpacity,
+    TextInput,
+    FlatList, ListRenderItem, Dimensions
 } from 'react-native';
 
 const {width} = Dimensions.get('screen')
@@ -23,101 +23,72 @@ const arrPhoneName = ['Huawei', 'Nokia', 'Samsung', 'IPhone', 'LG']
 const arrPrice = [200, 500, 1000, 1000, 400]
 
 const arrData = new Array(50).fill(null).map((_, index) => ({
-  id: index + 1,
-  phoneName: arrPhoneName[index % arrPhoneName.length],
-  price: arrPrice[index % arrPrice.length]
+    id: index + 1,
+    phoneName: arrPhoneName[index % arrPhoneName.length],
+    price: arrPrice[index % arrPrice.length]
 }))
 
 type DataType = {
-  id: number
-  phoneName: string
-  price: number
+    id: number
+    phoneName: string
+    price: number
 }
 
 export default function App() {
 
-  const [text, onChangeText] = useState("");
-  const [refreshing, setRefreshing] = useState(false);
+    const [text, onChangeText] = useState("");
+    const [refreshing, setRefreshing] = useState(false);
 
-  useEffect(() => {
-    setTimeout(() => {setRefreshing(false)}, 2000)
-  }, [refreshing])
+    useEffect(() => {
+        setTimeout(() => {
+            setRefreshing(false)
+        }, 2000)
+    }, [refreshing])
 
-  const renderHandler: ListRenderItem<DataType> = ({item}) => {
-    return <View style={styles.item}>
-      <Text numberOfLines={1}>{item.phoneName}</Text>
-      <Text>{item.price}</Text>
-    </View>
-  }
+    const renderHandler: ListRenderItem<DataType> = ({item}) => {
+        return <View style={styles.item}>
+            <Text numberOfLines={1}>{item.phoneName}</Text>
+            <Text>{item.price}</Text>
+        </View>
+    }
 
-  return (
-      <View style={styles.container}>
+    return (
+        <View style={styles.container}>
 
-        <FlatList
-            data={arrData}
-            renderItem={renderHandler}
-            numColumns={NUM_COL}
-            columnWrapperStyle={{justifyContent: 'space-between'}}
-            ListEmptyComponent={() => <View><Text>Текст если нечего нету</Text></View> }
-            ListHeaderComponent={() => <View><Text>Текст Header</Text></View>}
-            stickyHeaderIndices={[0]}
-            refreshing={refreshing}
-            onRefresh={() => {
-              setRefreshing(true)
-            }}
-            // keyExtractor={item => item.id}
-        />
-        {/*<Text>Open up App.tsx to start working on your app!</Text>*/}
-        {/*<ActivityIndicator/>*/}
-        {/*<Button*/}
-        {/*    title="Press me"*/}
-        {/*    onPress={() => Alert.alert('Simple Button pressed')}*/}
-        {/*/>*/}
-        {/*<Image*/}
-        {/*    style={{*/}
-        {/*        height: 50,*/}
-        {/*        width: 50*/}
-        {/*    }}*/}
-        {/*    source={{*/}
-        {/*        uri: 'https://reactnative.dev/img/tiny_logo.png',*/}
-        {/*    }}*/}
-        {/*/>*/}
-        {/*<TouchableOpacity*/}
-        {/*    // style={styles.button}*/}
-        {/*    onPress={() => {*/}
-        {/*    }}*/}
-        {/*>*/}
-        {/*    <Text>Press Here</Text>*/}
-        {/*</TouchableOpacity>*/}
-        {/*<TextInput*/}
-        {/*    style={{*/}
-        {/*        height: 40,*/}
-        {/*        width: 200,*/}
-        {/*        borderWidth: 1,*/}
-        {/*    }}*/}
-        {/*    onChangeText={onChangeText}*/}
-        {/*    value={text}*/}
-        {/*/>*/}
-      </View>
-  );
+            <FlatList
+                data={arrData}
+                renderItem={renderHandler}
+                numColumns={NUM_COL}
+                columnWrapperStyle={{justifyContent: 'space-between'}}
+                ListEmptyComponent={() => <View><Text>Текст если нечего нету</Text></View>}
+                ListHeaderComponent={() => <View><Text>Текст Header</Text></View>}
+                stickyHeaderIndices={[0]}
+                refreshing={refreshing}
+                onRefresh={() => {
+                    setRefreshing(true)
+                }}
+                // keyExtractor={item => item.id}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    marginTop: 35,
-    paddingHorizontal: 20,
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-  item: {
-    marginVertical: 5,
-    width: (WIDTH - 20 * 2) / NUM_COL - 5,
-    height: (WIDTH - 20 * 2) / NUM_COL - 5,
-    backgroundColor: 'tomato',
-    borderWidth: 1,
-    padding: 10,
-    borderRadius: 5
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        marginTop: 35,
+        paddingHorizontal: 20,
+        // alignItems: 'center',
+        // justifyContent: 'center',
+    },
+    item: {
+        marginVertical: 5,
+        width: (WIDTH - 20 * 2) / NUM_COL - 5,
+        height: (WIDTH - 20 * 2) / NUM_COL - 5,
+        backgroundColor: 'tomato',
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 5
+    }
 });
